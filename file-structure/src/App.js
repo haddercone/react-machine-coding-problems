d4df2953b2b97c7e7031df1e66f504193e945129
Robin Singh <44robin.rs@gmail.com>
2023-03-18 17:22:18 +0530

    implement a file structure in browser like VS code

---
+++ file-structure/src/App.js
@@ -0,0 +1,11 @@
+
+import FileTree from "./FileTree";
+
+function App() {
+  
+  return (
+    <FileTree />
+  );
+}
+
+export default App;
---
+++ file-structure/src/FileTree.js
@@ -0,0 +1,119 @@
+import { useState } from "react";
+function Child({ name, children, depth }) {
+    const [isVisible, setIsVisible] = useState(false)
+    const [file, setFile] = useState(children)
+    const [fileName, setFileName] = useState('')
+    const [isFileFieldVisible, setIsFileFieldvisible] = useState(false)
+    const [isFolderFieldVisible, setIsFolderFieldvisible] = useState(false)
+    function handleAddFile(e) {
+        e.preventDefault()
+        const data = {
+            name: fileName
+        }
+        setFile(prev => [...prev, data])
+        setIsFileFieldvisible(false)
+        setFileName("")
+    }
+    function handleAddFolder(e) {
+        e.preventDefault()
+        const data = {
+            name: fileName,
+            children: []
+        }
+        setFile(prev => [...prev, data])
+        setIsFolderFieldvisible(false)
+        setFileName("")
+    }
+    return <>
+        {file ?
+            <div style={{ marginLeft: `${depth}px` }}>
+                <button className="btn" onClick={() => setIsVisible(!isVisible)} >
+                    {isVisible ? "-" : "+"} {name}</button>
+                <button className="action" onClick={() => setIsFileFieldvisible(!isFileFieldVisible)}>Add file</button>
+                <button className="action" onClick={() => setIsFolderFieldvisible(!isFolderFieldVisible)}>Add folder</button>
+                {isFileFieldVisible &&
+                    <div>
+                        <form onSubmit={handleAddFile} >
+                            <input type="text" value={fileName} onChange={(e) => setFileName(e.target.value)} />
+                        </form>
+                    </div>
+                }
+                {isFolderFieldVisible &&
+                    <div>
+                        <form onSubmit={handleAddFolder} >
+                            <input type="text" value={fileName} onChange={(e) => setFileName(e.target.value)} />
+                        </form>
+                    </div>
+                }
+
+            </div> :
+            <div style={{ marginLeft: `${depth}px` }}>
+                {name}
+            </div>
+        }
+        {isVisible && file &&
+            file.map(child => (
+                <Child key={child.name} {...child} depth={depth + 10} />
+            ))
+        }
+    </>
+}
+const FileTree = () => {
+    const fileTree = [
+        {
+            name: "node_modules",
+            children: [{
+                name: ".bin",
+                children: [{
+                    name: "acron",
+
+                }]
+            }]
+        },
+        {
+            name: "src",
+            children: [{
+                name: "components",
+                children: [{
+                    name: "Head.js",
+
+                }]
+            }]
+        },
+        {
+            name: "assets",
+            children: [
+                {
+                    name: "icons",
+                    children: []
+                },
+                {
+                    name: "images",
+                    children: [
+                        {
+                            name: "Pic.jpeg"
+                        },
+                        {
+                            name: "Pic2.png"
+                        }
+                    ]
+                },
+
+            ]
+        },
+        {
+            name: "package.json"
+        }
+    ]
+    return (
+        <>
+            <div className="App">
+                {fileTree.map(element => (
+                    <Child key={element.name}{...element} depth={10} />
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default FileTree;
